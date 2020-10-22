cc551f64e020b17bd2aa9594be0eea07da8892af
Abhishek Nandan <54950560+nandanabhishek@users.noreply.github.com>
2020-10-22 13:32:52 +0530

    Add files via upload

---
+++ Linked List/DLL/Deletion in DLL/Deletion at the End in DLL/main.c
@@ -0,0 +1,33 @@
+
+// defining the structure of a node in DLL
+struct node
+{
+    struct node *prev;
+    int data;
+    struct node *next;
+
+};
+
+
+// function to delete element from the end of SLL
+struct node *delete_End(struct node *head) {
+    struct node *ptr, *temp;
+    ptr=head;
+    temp=head;
+
+    if(head == NULL) {
+        printf("SLL is empty!(Underflow!!)");
+        exit(0);
+    }
+    else {
+        while(ptr->next != NULL){
+            temp = ptr;
+            ptr = ptr->next;
+        }
+        temp->next = NULL;
+        free(ptr);
+
+        return head;
+    }
+
+}
