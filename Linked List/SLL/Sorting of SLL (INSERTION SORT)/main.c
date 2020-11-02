fd994987591c65f66fa215fd6959bc45b769f5af
Abhishek Nandan <abhisheknandan00@gmail.com>
2020-11-02 16:01:34 +0530

    Update main.c

--- Linked List/SLL/Sorting of SLL (INSERTION SORT)/main.c
+++ Linked List/SLL/Sorting of SLL (INSERTION SORT)/main.c
@@ -16,20 +16,27 @@ struct node *sort_list(struct node *head)
     struct node *ptr1, *ptr2;
     int temp;
     ptr1 = head;
-    while(ptr1->next != NULL)
-    {
-        ptr2 = ptr1 -> next;
-        while(ptr2 != NULL)
-        {
-            if(ptr1->data > ptr2->data)
-            {
-                temp = ptr1 -> data;
-                ptr1 -> data = ptr2 -> data;
-                ptr2 -> data = temp;
-            }
-            ptr2 = ptr2 -> next;
-        }
-        ptr1 = ptr1 -> next;
+    
+    if(head == NULL) {
+        return head;
     }
-    return head; // Had to be added
+    else {
+        while(ptr1->next != NULL)
+        {
+            ptr2 = ptr1 -> next;
+            while(ptr2 != NULL)
+            {
+                if(ptr1->data > ptr2->data)
+                {
+                    temp = ptr1 -> data;
+                    ptr1 -> data = ptr2 -> data;
+                    ptr2 -> data = temp;
+                }
+                ptr2 = ptr2 -> next;
+            }
+            ptr1 = ptr1 -> next;
+        }
+        return head; // Had to be added
+    }
+    
 }
