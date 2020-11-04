b8481490bafecb2171f0f6865a907800f6e8b3f7
Abhishek Nandan <abhisheknandan00@gmail.com>
2020-11-05 00:04:42 +0530

    Searching Minim

---
+++ Tree/Binary Search Tree/Searching Minimum element in BST/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+
+// defining structure of node of BST (Binary Search Tree)
+struct BSTNode {
+    struct BSTNode *left;
+    int info;
+    struct BSTNode *right;
+};
+
+struct BSTNode *root=NULL;
+
+
+
+// function for finding the minimum element in a Binary Search Tree
+// Recursive Approach
+struct BSTNode *find_Min(struct BSTNode *root)
+{
+    if (root == NULL)
+        return root;
+    else {
+        if (root->left == NULL)
+            return root;
+        else
+            find_Min(root->left);
+    }
+}
