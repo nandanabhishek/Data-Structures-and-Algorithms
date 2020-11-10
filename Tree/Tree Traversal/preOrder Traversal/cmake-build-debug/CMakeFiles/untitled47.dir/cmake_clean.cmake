da61dce3622f0fccb60c5c0c4bb92bd39401eddb
Abhishek Nandan <abhisheknandan00@gmail.com>
2020-11-10 20:21:57 +0530

    Add files via upload

---
+++ Tree/Tree Traversal/preOrder Traversal/cmake-build-debug/CMakeFiles/untitled47.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled47.dir/main.c.obj"
+  "libuntitled47.dll.a"
+  "untitled47.exe"
+  "untitled47.exe.manifest"
+  "untitled47.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled47.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
