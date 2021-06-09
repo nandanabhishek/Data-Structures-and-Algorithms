bb055b7cd2fbda4e2f55f5752a7d4417c40baae7
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-06-09 20:50:23 +0530

    Create sol.py

---
+++ Tree/Binary Search Tree/Search in BST/sol.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root:
+            if root.val==val:
+                return root
+            if root.val>val:
+                return self.searchBST(root.left, val)
+            else:
+                return self.searchBST(root.right, val)
+        return None
+                
+            
+            
+        
+        
