f40164f364caaaf00e93c28dd5ac7ab0edda10de
Abhishek Nandan <abhisheknandan00@gmail.com>
2021-12-06 20:04:01 +0530

    Create sol.py

---
+++ DP/House Robber II/sol.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # solving using single rob function of problem House Robber
+        def rob_helper(nums):
+            dp1, dp2 = 0, 0
+            for num in nums:
+                dp1, dp2 = dp2, max(dp1 + num, dp2)          
+            return dp2
+    
+        return max(nums[0] + rob_helper(nums[2:-1]), rob_helper(nums[1:]))
